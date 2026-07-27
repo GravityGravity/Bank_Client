1505a23541c8192b048d2b6af785a98e11948aec
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-07-27 13:16:27 -0400

    Change accounts struct from list to dict
    Add functionality to retrieve account object from account number instead of iterating through a list of indexes.
    Improve efficiency of account retrieval and management by using a dictionary to store accounts with account numbers as keys. This allows for O(1) average time complexity for lookups, additions, and deletions of accounts.

--- client.py
+++ client.py
@@ -7,18 +7,18 @@ class Client:
         self.f_name = f_name
         self.l_name = l_name
         self.user_id = user_id #Unique user ID for each client
-        self.accounts = [] # Review: Most likely will need to change this to a key-value pair to store account numbers and their corresponding balances
+        self.accounts: dict[int, Account] = {}
 
     def get_info(self):
         return f"Client Name: {self.f_name} {self.l_name}, User ID: {self.user_id}"
 
     def create_account(self, account_number):
-        if account_number in [account.account_number for account in self.accounts]:
+        if account_number in self.accounts:
             return f"Account number {account_number} already exists"
         new_account = Account(account_number)
-        self.accounts.append(new_account)
+        self.accounts[account_number] = new_account
         return f"Account {account_number} created" 
 
     def print_accounts(self):
-        account_info = [account.print_balance() for account in self.accounts]
+        account_info = [account.print_balance() for account in self.accounts.values()]
         return "\n".join(account_info)
