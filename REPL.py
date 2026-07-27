e8e4da948f71d5de59b5c201495e752420e920dd
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-07-27 14:08:48 -0400

    Create REPL, Create transaction class file
    Have to track transactions for each account.
    Create a transaction class to store transaction details like
    transaction ID, amount, type (deposit or withdrawal), and timestamp.
    Update the account class to create a transaction object whenever a deposit
    or withdrawal is made and store it in a list of transactions.

---
+++ REPL.py
@@ -0,0 +1,15 @@
+
+
+
+
+while(True):
+    try:
+        user_input = input("% ")
+        match user_input.lower().strip():
+            case "exit":
+                break
+            case _:
+                # Process the user input here
+                print(f"You entered: {user_input}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
---
+++ transaction.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+class Transaction:
+    def __init__(self, transaction_id: int, amount: float, transaction_type: str, balance_before_transaction: float):
+        self.transaction_id = transaction_id
+        self.amount = amount
+        self.transaction_type = transaction_type # 'deposit' or 'withdrawal'
+        self.balance_after_transaction += -amount if transaction_type == 'withdrawal' else amount  # This will be set after the transaction is processed
+        self.time = datetime.now()  # Timestamp of the transaction
