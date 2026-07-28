fb93996a0b208285391fc39459a07d50b56d1ae5
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2026-07-27 23:22:55 -0400

    Add basic parsing functionality to REPL

--- REPL.py
+++ REPL.py
@@ -1,14 +1,45 @@
 from client import Client
+import time
+import shlex
+
+client = None  # Client login will be stored within REPL file
+
+# Login or Create Account (CREATE/GET)
+# Print Clients accounts W/ Balances
+# Withdrawal from specific account
+# Depost from specific account
 
 
-while(True):
+# Intro startup
+print(r"""
+****************************************
+*                                      *
+*        Bank App (ALPHA)              *
+*                                      *
+****************************************
+""")
+time.sleep(1)
+
+
+while (True):
     try:
         user_input = input("% ")
-        match user_input.lower().strip():
-            case "exit":
-                break
-            case _:
-                # Process the user input here
-                print(f"You entered: {user_input}")
+
+        # Shlex Parses user input into {commands} {args...}
+        if user_input:
+            parsed_input = shlex.split(user_input)
+            command = parsed_input[0]
+            args = parsed_input[1:]
+
+            match command.lower():
+                case "exit":  # Close Application
+                    break
+                case _:
+                    # Process the user input here
+                    print(f"Entered: '{user_input}'")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def login():
+    return None
