9fda07deea5838bbab5a14c8e6836b84e3dbccc3
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2026-07-28 08:55:52 -0400

    Refactor entire codebase with new structure
    Use a DB instead of JSON to store client data.

--- account.py
+++ account.py
@@ -1,39 +1,18 @@
-# account.py
+from fastapi import FastAPI
+from app.routes.account_routes import router as account_router
 
-from transaction import Transaction
+app = FastAPI()
+app.include_router(account_router)
 
-class Account:
-    def __init__(self, account_number):
-        self.account_number = account_number # Unique account number for each account
-        self.__balance = 0
-        self.account = account_number
-        self.transactions = [] # List to store transactions for the account
+@app.get("/")
+def check_app():
+    return {"message" : "Bank API is running"}
 
-    def deposit(self, amount):
-        if amount <= 0:
-            return "Deposit amount must be positive"
-        self.__balance += amount
-        self.transactions.append(Transaction(len(self.transactions), amount, "deposit", self.__balance - amount))
-        return f"Deposited {amount}. New balance: ${self.__balance}"
+# repl users gonna use to communicate with the service via sevice file
+# service file basically translates 
 
-    def withdraw(self, amount):
-        if amount <= 0:
-            return "Withdrawal amount must be positive"
-        if amount > self.__balance:
-            return "Insufficient funds"
-        self.__balance -= amount
-        self.transactions.append(
-            Transaction(len(self.transactions), amount, "withdrawal", self.__balance + amount))
-        return f"Withdrew {amount}. New balance: ${self.__balance}"
 
-    def print_balance(self):
-        return f"Account {self.account_number} balance: ${self.__balance}"
-
-    def print_transactions(self, account_number):
-        if account_number != self.account_number:
-            return "Invalid account number"
-        if not self.transactions:
-            return "No transactions found"
-        
-        transaction_history = [f"Transaction ID: {t.transaction_id}, Type: {t.transaction_type}, Amount: {t.amount}, Balance After Transaction: {t.balance_after_transaction}, Time: {t.time}" for t in self.transactions]
-        return "\n".join(transaction_history)
+# bottom to top approach:
+# look into rpel first
+# package into services
+# then controllers
--- transaction.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-class Transaction:
-    def __init__(self, transaction_id: int, amount: float, transaction_type: str, balance_before_transaction: float):
-        self.transaction_id = transaction_id
-        self.amount = amount
-        self.transaction_type = transaction_type # 'deposit' or 'withdrawal'
-        self.balance_after_transaction = balance_before_transaction + (-amount if transaction_type == 'withdrawal' else amount)  # This will be set after the transaction is processed
-        self.time = datetime.now()  # Timestamp of the transaction
