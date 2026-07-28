a60a8c3d0dae3b2fefea004eaa7f685ce345d07d
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2026-07-28 08:51:50 -0400

    Misc. type bugfixes

--- client_list.py
+++ client_list.py
@@ -8,12 +8,12 @@ class Client_List:
     def __init__(self):
 
         self.clients: dict[int, Client] = {}
-        with open("client_info.JSON", "r") as file:
-            data = json.load(file)
-        for client_data in data:
-            client = Client(
-                client_data["f_name"], client_data["l_name"], client_data["user_id"])
-            self.clients[client.user_id] = client
+        # with open("client_info.JSON", "r") as file:
+        #     data = json.load(file)
+        # for client_data in data:
+        #     client = Client(
+        #         client_data["f_name"], client_data["l_name"], client_data["user_id"])
+        #     self.clients[client.user_id] = client
 
     def add_client(self, client):
         self.clients[client.user_id] = client
